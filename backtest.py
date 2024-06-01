6f44bc31679901a1bf276d3c9d096d9f76a49313
Etienne Sharpin <etienne.sharpin@epita.fr>
2024-06-01 18:55:10 +0200

    [FIX] formating + docstring

--- backtest.py
+++ backtest.py
@@ -4,8 +4,16 @@ import plotly.graph_objs as go
 
 
 class Backtest:
+    """
+    Load and plot historical stocks data for a given stock
+    """
 
     def __init__(self, csv_path: str, is_copy_pasted_csv: bool = False) -> None:
+        """
+        Args:
+            csv_path (str): path to the CSV containing data from yahoo finance, example: https://finance.yahoo.com/quote/%5EGSPC/history/?frequency=1mo&period1=1705528411&period2=1713386985
+            is_copy_pasted_csv (bool, optional): Did you copy pasted the data or was it downloaded. Defaults to False (downloaded).
+        """
         self.csv_path = csv_path
         if is_copy_pasted_csv:
             self.df = self._read_copy_pasted_csv()
@@ -13,7 +21,9 @@ class Backtest:
             self.df = self.read_downloaded_csv()
 
     def get_trace(self):
-        return go.Scatter(x=self.df['Date'], y=self.df['Open'], mode='lines', name=self.csv_path)
+        return go.Scatter(
+            x=self.df["Date"], y=self.df["Open"], mode="lines", name=self.csv_path
+        )
 
     def plot(self) -> None:
         trace = self.get_trace()
@@ -21,13 +31,15 @@ class Backtest:
         fig.add_trace(trace)
         fig.show()
 
-    def get_trace_DCA(self, monthly_invested: int) -> None:
+    def get_trace_DCA(self, monthly_invested: int) -> go.Scatter:
         df = self.df  # alias
         df["Shares bought"] = monthly_invested / df["Open"]
         df["Cumulative shares"] = df["Shares bought"].cumsum()
         df["Wallet value"] = df["Cumulative shares"] * df["Open"]
 
-        trace = go.Scatter(x=df['Date'], y=df['Wallet value'], mode='lines', name=self.csv_path)
+        trace = go.Scatter(
+            x=df["Date"], y=df["Wallet value"], mode="lines", name=self.csv_path
+        )
 
         return trace
 
@@ -46,25 +58,26 @@ class Backtest:
         https://finance.yahoo.com/quote/%5EGSPC/history/?frequency=1mo&period1=1705528411&period2=1713386985
         """
         schema = {
-            'Date': str,
-            'Open': np.float64,
-            'High': np.float64,
-            'Low': np.float64,
-            'Close': np.float64,
-            'Adj Close': np.float64,
-            'Volume': int,
+            "Date": str,
+            "Open": np.float64,
+            "High": np.float64,
+            "Low": np.float64,
+            "Close": np.float64,
+            "Adj Close": np.float64,
+            "Volume": int,
         }
 
-        df = pd.read_csv(self.csv_path,
-                    header=None,
-                    dtype=schema,
-                    names=schema.keys(),
-                    thousands=',',
-                    sep='\t')
+        df = pd.read_csv(
+            self.csv_path,
+            header=None,
+            dtype=schema,
+            names=list(schema.keys()),
+            thousands=",",
+            sep="\t",
+        )
 
-        df['Date'] = pd.to_datetime(df['Date'], format='%b %d, %Y')
+        df["Date"] = pd.to_datetime(df["Date"], format="%b %d, %Y")
 
         df = df.sort_values("Date").reset_index(drop=True)
 
         return df
-
--- backtest_comparator.py
+++ backtest_comparator.py
@@ -3,7 +3,11 @@ import pandas as pd
 
 from backtest import Backtest
 
+
 class BacktestComparator:
+    """
+    Compare several Backtest on the same plot
+    """
 
     def __init__(self, backtests: list[Backtest]) -> None:
         self.backtests = backtests
@@ -15,9 +19,9 @@ class BacktestComparator:
         for trace in traces:
             fig.add_trace(trace)
 
-        fig.update_layout(title='Stock comparison',
-                  xaxis_title='Date',
-                  yaxis_title='Share value')
+        fig.update_layout(
+            title="Stock comparison", xaxis_title="Date", yaxis_title="Share value"
+        )
         fig.show()
 
     def plot_DCA(self, monthly_invested: int) -> None:
@@ -26,16 +30,18 @@ class BacktestComparator:
         traces = [b.get_trace_DCA(monthly_invested) for b in self.backtests]
 
         # FIXME, better handle dates
-        dates = self.backtests[0].df['Date'].copy()
-        amounts = (dates.index+1) * monthly_invested
-        uninvested_trace = go.Scatter(x=dates, y=amounts, mode='lines', name='Uninvested cash')
+        dates = self.backtests[0].df["Date"].copy()
+        amounts = (dates.index + 1) * monthly_invested
+        uninvested_trace = go.Scatter(
+            x=dates, y=amounts, mode="lines", name="Uninvested cash"
+        )
 
         fig.add_trace(uninvested_trace)
 
         for trace in traces:
             fig.add_trace(trace)
 
-        fig.update_layout(title='DCA comparison',
-                  xaxis_title='Date',
-                  yaxis_title='Wallet value')
+        fig.update_layout(
+            title="DCA comparison", xaxis_title="Date", yaxis_title="Wallet value"
+        )
         fig.show()
